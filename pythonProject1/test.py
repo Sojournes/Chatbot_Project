83ed5593e4ff86c5af1b3ac4793e602f007b3a9b
Diwakar Sehgal <97471046+Sojournes@users.noreply.github.com>
2025-02-24 17:09:40 +0530

    Update test.py

--- pythonProject1/test.py
+++ pythonProject1/test.py
@@ -1,7 +1,6 @@
-#XQhUzb6Ln3wczkAgwLr6NzAHkrVHl8fTsgrAOUNTSGsIRT8ythQEPMNGjjIpodybYJG5jA.
 from bardapi import Bard
 
-token = 'XQhUzb6Ln3wczkAgwLr6NzAHkrVHl8fTsgrAOUNTSGsIRT8ythQEPMNGjjIpodybYJG5jA.'
+token = ""
 bard = Bard(token=token)
 response = bard.get_answer("What is Machine Learning?")['content']
 print(response)
